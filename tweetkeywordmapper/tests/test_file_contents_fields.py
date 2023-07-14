20b29959f781cfa79b785a6b02162b99ed9df539
Stephen C. Sanders <scsandman6496@gmail.com>
2023-07-14 15:35:00 -0400

    moved test directory to within tweetkeywordmapper package

---
+++ tweetkeywordmapper/tests/test_file_contents_fields.py
@@ -0,0 +1,13 @@
+from tweetkeywordmapper.core import data
+from tweetkeywordmapper.core import constants as cons
+
+ws = cons.workspace
+default_file = cons.default_file
+
+file, file_ext = data.get_user_file(default_file)
+contents, fields = data.get_file_contents_fields(file, file_ext)
+
+print()
+print(contents)
+print()
+print(f'Fields: {fields}')
